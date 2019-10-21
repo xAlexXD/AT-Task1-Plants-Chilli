d1c1f42a06ef2838f63da9fca2ccaf4569931219
Alexander Allman <xalexxd@hotmail.co.uk>
2019-10-21 20:43:05 +0100

    Sliders now working yeet, did a rebuild too

--- Task1ProPlants/source/PointLight.cpp
+++ Task1ProPlants/source/PointLight.cpp
@@ -16,16 +16,16 @@ void PointLight::SpawnControlWindow() noexcept
 		ImGui::SliderFloat("Y", &_cbData.pos.y, -60.0f, 60.0f, "%.1f");
 		ImGui::SliderFloat("Z", &_cbData.pos.z, -60.0f, 60.0f, "%.1f");
 
-		ImGui::Text("Intensity / Colour");
-		ImGui::SliderFloat("Intensity", &_cbData.diffuseIntensity, 0.01f, 2.0f, "%.2");
+		ImGui::Text("Intensity/Colour");
+		ImGui::SliderFloat("Intensity", &_cbData.diffuseIntensity, 0.01f, 2.0f, "%.2f", 2);
 		ImGui::ColorEdit3("Diffuse Colour", &_cbData.diffuseColor.x);
 		ImGui::ColorEdit3("Ambient Colour", &_cbData.ambient.x);
 		ImGui::ColorEdit3("Material Colour", &_cbData.materialColor.x);
 
 		ImGui::Text("Falloff");
-		ImGui::SliderFloat("Constant", &_cbData.attConst, 0.05f, 10.0f, "%.2");
-		ImGui::SliderFloat("Linear", &_cbData.attLin, 0.0001f, 4.0f, "%.4");
-		ImGui::SliderFloat("Quadratic", &_cbData.attQuad, 0.0000001f, 10.0f, "%.7");
+		ImGui::SliderFloat("Constant", &_cbData.attConst, 0.05f, 10.0f, "%.2f", 4);
+		ImGui::SliderFloat("Linear", &_cbData.attLin, 0.0001f, 4.0f, "%.4f", 8);
+		ImGui::SliderFloat("Quadratic", &_cbData.attQuad, 0.0000001f, 10.0f, "%.7f", 10);
 
 		if (ImGui::Button("Reset"))
 		{
