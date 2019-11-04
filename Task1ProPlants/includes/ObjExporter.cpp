80ecb9fb47777f2e2e45e1014251ede5fdf2cb5f
Alex Allman <xalexxd@hotmail.co.uk>
2019-11-04 23:32:25 +0000

    Set up cutting off extensions of textures for material names

--- Task1ProPlants/includes/ObjExporter.cpp
+++ Task1ProPlants/includes/ObjExporter.cpp
@@ -3,6 +3,46 @@
 
 void ObjExporter::ExportToObj(std::vector<std::vector<TexturedVertex>> vecVertVec, std::vector<std::vector<int>> vecIndVec, std::vector<std::string> texNames) noexcept
 {
+	//Set up names of the textures without the extension
+	std::vector<std::string> texNamesNoExtension(texNames.size(), "");
+	for (size_t i = 0; i < texNames.size(); i++)
+	{
+		texNamesNoExtension[i] = texNames[i].substr(0u, texNames[i].length() - 4);
+	}
+
+	//Open a file stream to output to obj
+
+	//the size of all 3 are going to be the same so start a for loop with the size of one of them
+		//First check if there are any verts stored in this vec, if so continue, else break out
+			//Tag the name of the Vert struct here with the tex name at the same index
+			//Loop and output the first vecVertVec v vn and vt
+			//Take the inds at the appropriate index and add 1 to all inds as obj start at 1 not 0
+			//Use this modified ind array to build f, remembering to also put back faces on 
+	//Once the loop is finished for all ones in the vector, just finish by closing the file for now
+	//Open another file for the mtl creating the material using the texNames as names for the mats and using the tex names to define texs in the mats
+
+
+	//std::ofstream outFileMtl("./exports/Flower.mtl");
+	//if (outFileMtl.is_open())
+	//{
+	//	for (auto& name : texNames)
+	//	{
+	//		//first define the name of the material
+	//		outFileMtl << "newmtl " << 
+	//	}
+	//}
+
+	/*
+	newmtl Textured
+   Ka 1.000 1.000 1.000
+   Kd 1.000 1.000 1.000
+   Ks 0.000 0.000 0.000
+   d 1.0
+   illum 2
+   map_Ka lemur.tga           # the ambient texture map
+   map_Kd lemur.tga           # the diffuse texture map (most of the time, it will
+	*/
+
 	//std::ofstream outFile("./exports/Flower.obj");
 	//if (outFile.is_open())
 	//{
