3bdcdbe0ac1071f832085d55df1c5ce6b9f2d50c
Alex Allman <xalexxd@hotmail.co.uk>
2020-03-05 19:47:44 +0000

    Bush exporter is now working FIX NORMALS

--- Task1ProPlants/source/Bushes.cpp
+++ Task1ProPlants/source/Bushes.cpp
@@ -55,4 +55,43 @@ void Bushes::SpawnImgui(Graphics& gfx)
 
 void Bushes::Export(Graphics& gfx, ObjExporter& exporter, const char* outDir)
 {
+	//Create a vector of vert vectors, ind vectors and texture string vectors
+	int totalReserve = _bushVector.size();	//+1 for the stem
+	std::vector<std::vector<TexturedVertex>> vecOfVertVecs(totalReserve, std::vector<TexturedVertex>());
+	std::vector<std::vector<int>> vecOfIndVecs(totalReserve, std::vector<int>());
+	std::vector<std::string> vecOfTexNames;
+
+	//Calc how many it needs to reserve and reserve that many in each
+	vecOfVertVecs.reserve(totalReserve);
+	vecOfIndVecs.reserve(totalReserve);
+	vecOfTexNames.reserve(totalReserve);
+
+	int currentOffsetVert = 0;
+
+	//Bushes
+	for (int i = 0; i < _bushVector.size(); ++i)
+	{
+		//for each bunch, add the verts and inds to the grouped vectors
+		_bushVector[i]->UpdateLocalVertAndInd(gfx);
+		vecOfTexNames.push_back(_bushVector[i]->_texName);
+
+		for (size_t j = 0; j < _bushVector[i]->_faceVerts.size(); ++j)
+		{
+			vecOfVertVecs[i].push_back(_bushVector[i]->_faceVerts[j]);
+		}
+
+		for (size_t j = 0; j < _bushVector[i]->_faceInds.size(); ++j)
+		{
+			vecOfIndVecs[i].push_back(_bushVector[i]->_faceInds[j] + currentOffsetVert);
+		}
+
+		currentOffsetVert = 0;
+		for (const auto& vec : vecOfVertVecs)
+		{
+			currentOffsetVert += vec.size();
+		}
+	}
+
+	//Push them into the obj exporter
+	exporter.ExportToObj("Bushes", outDir, std::move(vecOfVertVecs), std::move(vecOfIndVecs), std::move(vecOfTexNames));
 }
